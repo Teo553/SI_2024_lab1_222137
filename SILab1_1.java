2c80d4041f4a66c2e83e1de7e1f120ac7cb11e80
Teo553 <teoeftimov34@gmail.com>
2024-04-01 14:36:22 +0200

    Fixed merge conflict in bug2

--- SILab1_1.java
+++ SILab1_1.java
@@ -10,6 +10,7 @@ public class SILab1_1 {
             }
         }
         System.out.println(SILab1_1.filterOddLengthStrings(list));
+        System.out.println(SILab1_2.makeBigStringFrom(list));
         return result;
     }
 
